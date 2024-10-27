3cba5e4069aa3c0f1ea6c91603c4980b94a7f508
Guilherme P Silva <guilhermeps002@outlook.com>
2024-10-27 05:51:36 -0300

    feat: mapeamento ECF bloco Y

---
+++ src/dto/ecf/blocoY/BlocoY.dto.ts
@@ -0,0 +1,43 @@
+import {
+    RegistroY001Dto,
+    RegistroY520Dto,
+    RegistroY570Dto,
+    RegistroY590Dto,
+    RegistroY600Dto,
+    RegistroY612Dto,
+    RegistroY620Dto,
+    RegistroY630Dto,
+    RegistroY640Dto,
+    RegistroY650Dto,
+    RegistroY660Dto,
+    RegistroY672Dto,
+    RegistroY680Dto,
+    RegistroY681Dto,
+    RegistroY682Dto,
+    RegistroY720Dto,
+    RegistroY750Dto,
+    RegistroY800Dto,
+    RegistroY990Dto,
+} from "./RegistrosY.dto";
+
+export default class BlocoYDto {
+    registroY001: RegistroY001Dto;
+    registroY520: RegistroY520Dto[];
+    registroY570: RegistroY570Dto[];
+    registroY590: RegistroY590Dto[];
+    registroY600: RegistroY600Dto[];
+    registroY612: RegistroY612Dto[];
+    registroY620: RegistroY620Dto[];
+    registroY630: RegistroY630Dto[];
+    registroY640: RegistroY640Dto[];
+    registroY650: RegistroY650Dto[];
+    registroY660: RegistroY660Dto[];
+    registroY672: RegistroY672Dto[];
+    registroY680: RegistroY680Dto[];
+    registroY681: RegistroY681Dto[];
+    registroY682: RegistroY682Dto[];
+    registroY720: RegistroY720Dto[];
+    registroY750: RegistroY750Dto[];
+    registroY800: RegistroY800Dto[];
+    registroY990: RegistroY990Dto;
+}
