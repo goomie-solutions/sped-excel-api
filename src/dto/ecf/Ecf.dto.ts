54a48c27eee5efe3dea93a7a6a21117b8ab6c7ef
Guilherme P Silva <guilhermeps002@outlook.com>
2024-10-30 20:16:19 -0300

    feat: ecf dto finalizado

---
+++ src/dto/ecf/Ecf.dto.ts
@@ -0,0 +1,37 @@
+import Bloco0Dto from "./bloco0/Bloco0.dto";
+import Bloco9Dto from "./bloco9/Bloco9.dto";
+import BlocoCDto from "./blocoC/BlocoC.dto";
+import BlocoEDto from "./blocoE/BlocoE.dto";
+import BlocoJDto from "./blocoJ/BlocoJ.dto";
+import BlocoKDto from "./blocoK/BlocoK.dto";
+import BlocoLDto from "./blocoL/BlocoL.dto";
+import BlocoMDto from "./blocoM/BlocoM.dto";
+import BlocoNDto from "./blocoN/BlocoN.dto";
+import BlocoPDto from "./blocoP/BlocoP.dto";
+import BlocoQDto from "./blocoQ/BlocoQ.dto";
+import BlocoTDto from "./blocoT/BlocoT.dto";
+import BlocoUDto from "./blocoU/BlocoU.dto";
+import BlocoVDto from "./blocoV/BlocoV.dto";
+import BlocoWDto from "./blocoW/BlocoW.dto";
+import BlocoXDto from "./blocoX/BlocoX.dto";
+import BlocoYDto from "./blocoY/BlocoY.dto";
+
+export default class EcfDto {
+    bloco0: Bloco0Dto;
+    blocoC: BlocoCDto;
+    blocoE: BlocoEDto;
+    blocoJ: BlocoJDto;
+    blocoK: BlocoKDto;
+    blocoL: BlocoLDto;
+    blocoM: BlocoMDto;
+    blocoN: BlocoNDto;
+    blocoP: BlocoPDto;
+    blocoQ: BlocoQDto;
+    blocoT: BlocoTDto;
+    blocoU: BlocoUDto;
+    blocoV: BlocoVDto;
+    blocoW: BlocoWDto;
+    blocoX: BlocoXDto;
+    blocoY: BlocoYDto;
+    bloco9: Bloco9Dto;
+}
