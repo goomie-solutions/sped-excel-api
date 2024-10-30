6568b28353ee786e0a92a432e66c4db897746381
Guilherme P Silva <guilhermeps002@outlook.com>
2024-10-30 20:04:21 -0300

    feat: mapeamento ECD bloco 9

---
+++ src/dto/ecd/bloco9/Bloco9.dto.ts
@@ -0,0 +1,8 @@
+import * as Registros9 from "./Registros9.dto";
+
+export default class Bloco9Dto {
+    registro9001: Registros9.Registro9001Dto;
+    registro9900: Registros9.Registro9900Dto[];
+    registro9990: Registros9.Registro9990Dto;
+    registro9999: Registros9.Registro9999Dto;
+}
---
+++ src/dto/ecd/bloco9/Registros9.dto.ts
@@ -0,0 +1,17 @@
+export class Registro9001Dto {
+    idRegistro: string = "9001";
+    indMov: string;
+}
+export class Registro9900Dto {
+    idRegistro: string = "9900";
+    registro: string;
+    totalRegistros: string;
+}
+export class Registro9990Dto {
+    idRegistro: string = "9990";
+    qtdRegistros: string;
+}
+export class Registro9999Dto {
+    idRegistro: string = "9999";
+    qtdRegistrosTotal: string;
+}
