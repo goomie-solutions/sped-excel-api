61f29c34262f8a57e1931a28ad2933bb4f5a83e3
Guilherme P Silva <guilhermeps002@outlook.com>
2024-10-30 19:48:03 -0300

    feat: mapeamento ECD bloco J

---
+++ src/dto/ecd/blocoJ/BlocoJ.dto.ts
@@ -0,0 +1,17 @@
+import * as RegistrosJ from "./RegistrosJ.dto";
+
+export default class BlocoJDto {
+    registroJ001: RegistrosJ.RegistroJ001Dto;
+    registroJ005: RegistrosJ.RegistroJ005Dto[];
+    registroJ100: RegistrosJ.RegistroJ100Dto[];
+    registroJ150: RegistrosJ.RegistroJ150Dto[];
+    registroJ210: RegistrosJ.RegistroJ210Dto[];
+    registroJ215: RegistrosJ.RegistroJ215Dto[];
+    registroJ800: RegistrosJ.RegistroJ800Dto[];
+    registroJ801: RegistrosJ.RegistroJ801Dto[];
+    registroJ900: RegistrosJ.RegistroJ900Dto[];
+    registroJ930: RegistrosJ.RegistroJ930Dto[];
+    registroJ932: RegistrosJ.RegistroJ932Dto[];
+    registroJ935: RegistrosJ.RegistroJ935Dto[];
+    registroJ990: RegistrosJ.RegistroJ990Dto;
+}
